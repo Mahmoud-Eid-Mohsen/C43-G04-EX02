045f7ff7bac9b562839d01156a53c0ab2cfabcaf
admin <admin@DESKTOP-JKD834T>
2025-02-04 17:09:44 +0200

    Add project files.

---
+++ Exam/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Exam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to the Examination System");
+            Console.WriteLine("-------------------------------------------\n");
+            int mainChoice;
+
+
+            do
+            {
+                Console.WriteLine("Please choose a type of exam:");
+                
+
+                Console.WriteLine("1. Final Exam | 2. Practical Exam  | 3. Exisit");
+               
+                Console.Write("Choose an option: ");
+            }
+            while (!int.TryParse(Console.ReadLine(), out mainChoice) || (mainChoice != 1 && mainChoice != 2&& mainChoice != 3));
+
+
+            Console.Clear();
+          
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exam/Questions/Question.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam.Questions
+{
+    public class Question
+    {
+       
+        public string Header { get; set; }
+
+       
+        public string Body { get; set; }
+
+        
+        public int Marks { get; set; }
+
+        
+        public Question(string header, string body, int marks)
+        {
+            Header = header;
+            Body = body;
+            Marks = marks;
+        }
+
+   
+        public override string ToString()
+        {
+            return $"{Header}\n{Body}\nMarks: {Marks}";
+        }
+    }
+}
